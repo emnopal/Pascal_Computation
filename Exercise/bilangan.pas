b83e5c9d87ddb3a2c7d97209da7a706a381d4117
Muhammad Naufal <mnaufal0999@gmail.com>
2019-07-06 09:42:01 +0700

    Add files via upload

---
+++ Exercise/bilangan.pas
@@ -0,0 +1,11 @@
+program bilangan;
+
+var
+    b: integer;
+
+begin
+    writeln('enter a number');
+    readln(b);
+    if (b>=0) then writeln('bilangan positif')
+    else writeln('bilangan negatif');
+end.
